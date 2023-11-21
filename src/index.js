174e249e5665961cec1fd4ab4f2afc942d23f73f
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-11-21 13:33:58 +0000

    Ingrid is the new input for name to test a timeline

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ console.log('Hi Cohort 13! Show me the commits!awdadwawdawdadw');
 let veronica = 'Veronica';
 let ingrid = 'Ingrid';
 
-let inputName = 'Veronica';
+let inputName = 'Ingrid';
 
 if (inputName === veronica) {
   console.log('Aiii caramba!');

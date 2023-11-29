3aaa96658a9f8c94f1f78df2dd5d707bb3bbd60e
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-11-29 09:23:48 +0000

    Show class how to programmatically create a button and change styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 const newP = document.createElement('p');
 newP.innerText = 'Bla';
 document.querySelector('body').appendChild(newP);
+
+const button1 = document.createElement('button');
+button1.innerText = 'Click me, dude!';
+document.querySelector('body').appendChild(button1);
+button1.classList.add('btn', 'btn-warning');

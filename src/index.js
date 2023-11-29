2962b94c98ea7c245fe40d4455a2d341297ae80a
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-11-29 10:13:07 +0000

    Veronica adds a second button that changes the text of the first

--- src/index.js
+++ src/index.js
@@ -11,3 +11,17 @@ button1.addEventListener('click', () => {
   console.log('You clicked me!');
   button1.innerText = 'You clicked me!';
 });
+
+// If you click on button2, it will change the text of button1
+button1.addEventListener('click', () => {
+  button1.innerText = 'You pushed me dude!';
+});
+const button2 = document.createElement('button');
+button2.innerText = 'Click to follow up';
+document.querySelector('body').appendChild(button2);
+button2.classList.add('btn', 'btn-success');
+
+button2.addEventListener('click', () => {
+  console.log('You clicked me!');
+  button1.innerText = 'Click to follow up';
+});

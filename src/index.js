d192d75b48861d873dc21d659d09089e8faece8a
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-11-29 09:52:05 +0000

    Show class how to add actions to buttons

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ const button1 = document.createElement('button');
 button1.innerText = 'Click me, dude!';
 document.querySelector('body').appendChild(button1);
 button1.classList.add('btn', 'btn-success');
+
+button1.addEventListener('click', () => {
+  console.log('You clicked me!');
+  button1.innerText = 'You clicked me!';
+});

3eac0edbbc9307866c90e8bfe8f100b9293d3fc9
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-11-29 09:36:39 +0000

    This makes Veronica more successful

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ document.querySelector('body').appendChild(newP);
 const button1 = document.createElement('button');
 button1.innerText = 'Click me, dude!';
 document.querySelector('body').appendChild(button1);
-button1.classList.add('btn', 'btn-warning');
+button1.classList.add('btn', 'btn-success');

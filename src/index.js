a766e0422c96cd610a8f2374de5423c405455a1d
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-11-30 09:21:33 +0000

    Ingrid refactors first button into a function for better overview

--- src/index.js
+++ src/index.js
@@ -38,13 +38,17 @@ button1.addEventListener('click', () => {
   counterElementShow.innerHTML = counter;
 });
 
-// Decrease the counter by 1 when the button is clicked
-const button2 = document.createElement('button');
-button2.innerText = 'Decrease';
-document.querySelector('body').appendChild(button2);
-button2.classList.add('btn', 'btn-dark');
+function createButton() {
+  const button = document.createElement('button');
 
-button2.addEventListener('click', () => {
-  counter--;
-  counterElementShow.innerHTML = counter;
-});
+  button.innerText = 'Decrease';
+  document.querySelector('body').appendChild(button);
+  button.classList.add('btn', 'btn-dark');
+
+  button.addEventListener('click', () => {
+    counter--;
+    counterElementShow.innerHTML = counter;
+  });
+}
+
+createButton();

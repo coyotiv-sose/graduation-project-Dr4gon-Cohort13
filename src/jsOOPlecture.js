e4a9ffa923f808a35cb0ec738322b0f345022d95
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-12-01 09:38:52 +0000

    Cohort13 defines riversurf model

---
+++ src/jsOOPlecture.js
@@ -0,0 +1,44 @@
+console.log('Namaste students, warm welcome to my riversurf academy 🏄🏾‍♂️🏄🏾‍♀️');
+// how to join Academy?
+//1) Internet 2) social interaction
+
+//which information is needed from user side?
+// nickname, name , weight, age, height, email, location
+
+// ---------- translates into the following user stories: ----------
+
+// User profile/login
+// User fills form with nickname, name , weight, birth date, height, email, location to register
+
+// User can login with nickname and password
+// User can join community
+// User can see his or another profile
+// User is able to upload video
+// User is able to delete account
+// User updates his profile
+
+//what is needed?
+//1) wet suit 2) board 3) helmet and west (optional)
+
+// ---------- translates into the following user stories: ----------
+
+// Basic equipment for riversurfing
+// User can see different suits and pick one according to their fit and the season they want to surf in
+// User can see different boards and pick one according to their fit
+// User can check sizes for helmet and west
+
+// levels
+// beginner => to be able to stand on a board
+// advanced =>  to be able to turn with a board
+// pro  => to be able to do tricks on board in any kind of situation
+// community
+//  friendly , arrogant
+//which type of Board is suggested for beginners
+// depends on wave
+//opening hours
+
+// ---------- translates into the following user stories: ----------
+
+// User filters wave level to pick the right wave for the start
+// User is shown friendly community, and board suggestions
+// User is able to look opening hours of the riverwave

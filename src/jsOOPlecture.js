253d338f784c909c55037fe10d551c4bedaced90
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-12-04 10:36:19 +0000

    De Long adds test for equipment board class attributes to show class testing

--- src/jsOOPlecture.js
+++ src/jsOOPlecture.js
@@ -175,3 +175,9 @@ console.log(
     deLong
   )}`
 );
+
+console.log(`------------------------------ Equipment Board Tests -------------------------------------------------`);
+console.log('Verify that Tuna board is from brand Wuux: ' + tuna.brand);
+console.log('Verify that Tun has 4.5 length: ' + tuna.length);
+console.log('Verify that Fingerling is a hard top board: ' + fingerling.type);
+console.log('Verify that Fingerling is named Shark model: ' + fingerling.modelName);

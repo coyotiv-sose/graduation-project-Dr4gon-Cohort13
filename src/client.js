967dbbaf5ef376970a58b18729c56046ad719e01
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2023-12-14 13:40:38 +0000

    Create example of transforming raw data into a class in the client

--- src/client.js
+++ src/client.js
@@ -1,4 +1,5 @@
 const Axios = require('axios');
+const Person = require('./person');
 
 const client = Axios.create({
   baseURL: 'http://localhost:3000',
@@ -13,6 +14,10 @@ async function getUsers() {
     // console.log(users);
     let emails = users.map(user => user.email);
     console.log(emails);
+
+    const johnInfos = users[0];
+    let john = new Person('Johnny', johnInfos.name, 42, 1.8, 80, '14-12-2023', johnInfos.email, 'Munich');
+    console.log(john);
   } catch (error) {
     console.error(error);
   }

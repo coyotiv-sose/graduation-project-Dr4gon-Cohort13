6eab5e74e44c56e6ac4d8192b73fd77a18a836d4
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-02 11:30:56 +0000

    Set up simple sync and async example

--- src/client.js
+++ src/client.js
@@ -1,26 +1,31 @@
 const Axios = require('axios');
 const Person = require('./person');
 
-const client = Axios.create({
-  baseURL: 'http://localhost:3000',
-  timeout: 1000,
-  headers: { 'X-Custom-Header': 'foobar' },
-});
+class Client {
+  static connect() {
+    this.client = Axios.create({
+      baseURL: 'http://localhost:3000',
+      timeout: 1000,
+      headers: { 'X-Custom-Header': 'foobar' },
+    });
+  }
 
-async function getUsers() {
-  try {
-    const response = await client.get('/users');
-    let users = response.data.users;
-    // console.log(users);
-    let emails = users.map(user => user.email);
-    console.log(emails);
+  static async getUsers() {
+    try {
+      const response = await this.client.get('/users');
+      let users = response.data.users;
+      // console.log(users);
+      let emails = users.map(user => user.email);
+      console.log(emails);
 
-    const johnInfos = users[0];
-    let john = new Person('Johnny', johnInfos.name, 42, 1.8, 80, '14-12-2023', johnInfos.email, 'Munich');
-    console.log(john);
-  } catch (error) {
-    console.error(error);
+      const johnInfos = users[0];
+      let john = new Person('Johnny', johnInfos.name, 42, 1.8, 80, '14-12-2023', johnInfos.email, 'Munich');
+      console.log(john);
+      return users;
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
-getUsers();
+module.exports = Client;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const client = require('./client');
+
+// client.connect();
+// const users = client.getUsers();
+// console.log('Send mail to; ', users[0].email);
+
+console.log('Sync communication is starting');
+console.log('Hi, I am the client');
+console.log('I am waiting for the server to answer');
+console.log('Async response from the server');
+console.log('Sync communication is over');

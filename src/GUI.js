d096f973dff5e1cb0936d1e80a190d0148a770ab
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-06 14:17:24 +0000

    Set default db for everyone

--- src/GUI.js
+++ src/GUI.js
@@ -1,6 +1,6 @@
 const client = require('./client');
 
 client.connect();
-client.getUsers().then(users => console.log('GUI retrieval of users: ', users));
 client.postUser().then(user => console.log('GUI creation of user: ', user));
+client.getUsers().then(users => console.log('GUI retrieval of users: ', users));
 client.deleteUser().then(user => console.log('GUI deletion of user: ', user));

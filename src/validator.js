7d7267d06bf85909e29b929ef6157de7073194e4
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-11 09:54:46 +0000

    Test password length for a minimum of 8 to be successful

--- src/validator.js
+++ src/validator.js
@@ -2,6 +2,10 @@
 
 class Validator {
   static validatePassword(password) {
+    if (password.length >= 8) {
+      return true;
+    }
+
     return false;
   }
 }
--- test/validator.test.js
+++ test/validator.test.js
@@ -5,3 +5,9 @@ test('If the password is empty, the validation should fail', () => {
   const expectedResult = false;
   expect(actualResult).toBe(expectedResult);
 });
+
+test('If the password is minimum 8 characters, the validation should succeed', () => {
+  const actualResult = Validator.validatePassword('12345678');
+  const expectedResult = true;
+  expect(actualResult).toBe(expectedResult);
+});

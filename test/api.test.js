d96bce9772d337ee16f29f99466a5f34251bfa1a
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-11 13:00:08 +0000

    Create example to check for number of items coming back

--- test/api.test.js
+++ test/api.test.js
@@ -3,6 +3,7 @@ const app = require('../src/app');
 
 test('Return all users from my app successfully', async () => {
   const response = await request(app).get('/users').expect(200);
-  // const expectedResult = false;
-  // expect(response.body.isValid).toBe(expectedResult);
+  console.log(response.body);
+  // I expect the number of users from my db to be 4
+  expect(response.body.length).toBe(4); // This will fail for u unless u have 4 users in ur db
 });

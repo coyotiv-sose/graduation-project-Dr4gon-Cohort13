fd64a5b6018b9fbfc17d0b0ee636a609256ce0a0
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-15 12:52:25 +0000

    Create example how to add a new view to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/test', // the url u type in browser such as localhost:5137/test
+      name: 'test',
+      component: () => import('../views/TestView.vue')
     }
   ]
 })

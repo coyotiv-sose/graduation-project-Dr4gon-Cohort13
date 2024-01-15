ee9f572938fe97c68c6933db643a8108a022ef2c
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-15 10:19:01 +0000

    Establish connection between frontend and backend to show class how communication works

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var boardsRouter = require('./routes/boards.js');
+const cors = require('cors');
 
 require('dotenv').config();
 require('./database-connection.js');
@@ -15,6 +16,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());

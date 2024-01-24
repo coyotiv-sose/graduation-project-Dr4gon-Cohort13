7880ec15c356c5e1db596a76a4b188598ebcbd18
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-24 07:39:09 +0000

    Setup inital theater entity

--- backend/src/mongo-aggregation.js.js
+++ backend/src/mongo-aggregation.js.js
@@ -3,18 +3,18 @@ const config = require('dotenv').config();
 
 const Movie = require('./model/aggregation/movie');
 const Comment = require('./model/aggregation/comment');
+const Theater = require('./model/aggregation/theater');
 
 // use ur connection string with the loadad sample data
 mongoose
-  .connect(
-    ''
-  )
+  .connect('')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Could not connect to MongoDB', err));
 
 async function main() {
   // console.log(await Movie.find().limit(5));
-  console.log(await Comment.find().limit(5));
+  // console.log(await Comment.find().limit(5));
+  console.log(await Theater.find().limit(5));
 }
 
 main();

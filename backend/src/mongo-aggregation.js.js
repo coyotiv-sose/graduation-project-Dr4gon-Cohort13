9666e353c81f3c0ac1691a1c2b1630a05b7cccae
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-24 07:26:17 +0000

    Update aggregration examples with comment

--- backend/src/mongo-aggregation.js.js
+++ backend/src/mongo-aggregation.js.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose');
 const config = require('dotenv').config();
 
 const Movie = require('./model/aggregation/movie');
+const Comment = require('./model/aggregation/comment');
 
 // use ur connection string with the loadad sample data
 mongoose
-  .connect('')
+  .connect(
+    ''
+  )
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Could not connect to MongoDB', err));
 
 async function main() {
-  console.log(await Movie.find().limit(5));
+  // console.log(await Movie.find().limit(5));
+  console.log(await Comment.find().limit(5));
 }
 
 main();

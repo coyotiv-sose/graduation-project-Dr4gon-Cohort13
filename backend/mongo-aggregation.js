b283b2496e23582f2439b0f6b3da4d2c93e93667
De Long Iu <4116724+Dr4gon@users.noreply.github.com>
2024-01-25 09:15:43 +0000

    Show simple example aggregation example with group and sort stage

--- backend/mongo-aggregation.js
+++ backend/mongo-aggregation.js
@@ -7,15 +7,24 @@ const Theater = require('./src/model/aggregation/theater');
 
 async function main() {
   // use ur connection string with the loaded sample data
-  await mongoose.connect('');
+  await mongoose.connect(
+    'mongodb+srv://cohort13:0DlSgV5kGHIBaINY@cohort13.c9kc8z7.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+  );
 
   // awaited
   console.log('Connected to MongoDB');
 
-  // Assumption: The US has 300 theaters
-  // Question: How many theaters are there in the US?
-  const response = await Theater.find();
-  console.log(response);
+  // Assumption: I want to build my own theater.
+  // Question: Which state has the least amount of theaters?
+  await calcStateWithLeastAmountOfTheaters();
 }
 
 main();
+
+async function calcStateWithLeastAmountOfTheaters() {
+  const response = await Theater.aggregate([
+    { $group: { _id: '$location.address.state', theaters: { $push: '$theaterId' }, numberOfTheaters: { $sum: 1 } } },
+    { $sort: { numberOfTheaters: -1 } },
+  ]);
+  console.log(response);
+}
